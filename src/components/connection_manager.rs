81586ebb815557a33e6bf48d24f639b42775095d
ronnakamoto <adarshron@Adarshs-MacBook-Air.local>
2024-07-07 16:47:31 +0530

    feat: updated connection manager ux

--- src/components/connection_manager.rs
+++ src/components/connection_manager.rs
@@ -1,7 +1,7 @@
 use crate::components::{Component, ThemedButton};
 use crate::models::{ConnectionProfile, ConnectionProfileManager};
 use crate::theme::Theme;
-use egui::{Context, RichText, Ui, Widget, Window};
+use egui::{Align, Context, Layout, RichText, Ui, Widget, Window};
 use std::cell::RefCell;
 use std::rc::Rc;
 use std::sync::Arc;
@@ -37,42 +37,72 @@ impl ConnectionManager {
     fn render_dialog_content(&mut self, ui: &mut Ui) {
         let profiles = self.profile_manager.borrow().get_profiles().to_vec();
 
-        // Render profile list
-        egui::ScrollArea::vertical().show(ui, |ui| {
-            for profile in profiles.iter() {
-                ui.horizontal(|ui| {
-                    if ui.button(&profile.name).clicked() {
-                        self.selected_profile = Some(profile.clone());
-                        self.connection_string = profile.connection_string.clone();
-                    }
-                    if ui.button("Edit").clicked() {
-                        self.new_profile = profile.clone();
-                        self.edit_mode = true;
-                    }
-                    if ui.button("Delete").clicked() {
-                        self.profile_manager
-                            .borrow_mut()
-                            .delete_profile(&profile.id);
-                    }
-                });
-            }
-        });
+        if profiles.is_empty() {
+            ui.vertical_centered(|ui| {
+                ui.add_space(20.0);
+                ui.label(
+                    RichText::new("No profiles exist. Please create a connection profile.")
+                        .color(self.theme.text_color)
+                        .size(16.0),
+                );
+                ui.add_space(20.0);
+            });
+        } else {
+            // Render profile list
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                for profile in profiles.iter() {
+                    ui.horizontal(|ui| {
+                        if ui.button(&profile.name).clicked() {
+                            self.selected_profile = Some(profile.clone());
+                            self.connection_string = profile.connection_string.clone();
+                        }
+                        if ThemedButton::new("Edit", Arc::clone(&self.theme))
+                            .ui(ui)
+                            .clicked()
+                        {
+                            self.new_profile = profile.clone();
+                            self.edit_mode = true;
+                        }
+                        if ThemedButton::new("Delete", Arc::clone(&self.theme))
+                            .ui(ui)
+                            .clicked()
+                        {
+                            self.profile_manager
+                                .borrow_mut()
+                                .delete_profile(&profile.id);
+                        }
+                    });
+                }
+            });
+        }
 
         ui.separator();
 
         // Render add/edit form
         ui.horizontal(|ui| {
-            ui.label("Name:");
-            ui.text_edit_singleline(&mut self.new_profile.name);
+            ui.add_sized([100.0, 20.0], egui::Label::new("Name:"));
+            ui.add_sized(
+                [200.0, 20.0],
+                egui::TextEdit::singleline(&mut self.new_profile.name),
+            );
         });
+        ui.add_space(5.0);
         ui.horizontal(|ui| {
-            ui.label("Connection String:");
-            ui.text_edit_singleline(&mut self.new_profile.connection_string);
+            ui.add_sized([100.0, 20.0], egui::Label::new("Connection String:"));
+            ui.add_sized(
+                [200.0, 20.0],
+                egui::TextEdit::singleline(&mut self.new_profile.connection_string),
+            );
         });
 
-        ui.horizontal(|ui| {
+        ui.add_space(10.0);
+
+        ui.with_layout(Layout::right_to_left(Align::TOP), |ui| {
             if self.edit_mode {
-                if ui.button("Update").clicked() {
+                if ThemedButton::new("Update Profile", Arc::clone(&self.theme))
+                    .ui(ui)
+                    .clicked()
+                {
                     self.profile_manager
                         .borrow_mut()
                         .save_profile(&self.new_profile);
@@ -84,7 +114,10 @@ impl ConnectionManager {
                     };
                 }
             } else {
-                if ui.button("Add").clicked() {
+                if ThemedButton::new("Add New Profile", Arc::clone(&self.theme))
+                    .ui(ui)
+                    .clicked()
+                {
                     self.new_profile.id = Uuid::new_v4().to_string();
                     self.profile_manager
                         .borrow_mut()
@@ -96,7 +129,10 @@ impl ConnectionManager {
                     };
                 }
             }
-            if ui.button("Cancel").clicked() {
+            if ThemedButton::new("Cancel", Arc::clone(&self.theme))
+                .ui(ui)
+                .clicked()
+            {
                 self.edit_mode = false;
                 self.new_profile = ConnectionProfile {
                     id: String::new(),
